7094fff4b87d791ce741884a21200f6a923d8f4a
tterrag1098 <tterrag1098@gmail.com>
2014-05-30 18:47:35 -0400

    Fix permissions derp where controllers were in the map.

--- src/main/java/tterrag/tppibot/registry/PermRegistry.java
+++ src/main/java/tterrag/tppibot/registry/PermRegistry.java
@@ -26,35 +26,35 @@ public class PermRegistry
      * {@link Channel} -> ({@link User} -> {@link PermLevel})
      */
     private Map<String, Map<String, PermLevel>> registrar;
-    
+
     private Set<String> controllers;
-    
+
     private Config registrarConfig, controllersConfig;
-    
-    private final String[] defaultControllers = new String[]{"tterrag"};
-    
+
+    private final String[] defaultControllers = new String[] { "tterrag" };
+
     public PermRegistry()
     {
         registrarConfig = new Config("permRegistry.json");
         controllersConfig = new Config("controllers.json");
-        
-        registrar = new Gson().fromJson(registrarConfig.getText(), new TypeToken<Map<String, Map<String, PermLevel>>>(){}.getType());
+
+        registrar = new Gson().fromJson(registrarConfig.getText(), new TypeToken<Map<String, Map<String, PermLevel>>>() {}.getType());
         controllers = new Gson().fromJson(controllersConfig.getText(), new TypeToken<Set<String>>() {}.getType());
 
         registrar = registrar == null ? new HashMap<String, Map<String, PermLevel>>() : registrar;
         controllers = controllers == null ? new HashSet<String>() : controllers;
-        
+
         if (controllers.isEmpty())
             controllers.addAll(Arrays.asList(defaultControllers));
     }
-    
+
     private static final PermRegistry INSTANCE = new PermRegistry();
-    
+
     public static PermRegistry instance()
     {
         return INSTANCE;
     }
-    
+
     public void registerUser(Channel chan, User user, PermLevel level)
     {
         // controllers are global
@@ -62,42 +62,42 @@ public class PermRegistry
         {
             controllers.add(IRCUtils.getAccount(user));
         }
-        
-        // can't assign op/voice
-        if (!ArrayUtils.contains(PermLevel.getSettablePermLevels(), level))
+        else
         {
-            throw new IllegalArgumentException("Cannot register a user with the level " + level.toString());
+            // can't assign op/voice
+            if (!ArrayUtils.contains(PermLevel.getSettablePermLevels(), level))
+                throw new IllegalArgumentException("Cannot register a user with the level " + level.toString());
+
+            String chanName = chan.getName();
+            String acct = IRCUtils.getAccount(user);
+
+            registrar.put(chanName, register(registrar.get(chanName), acct, level));
         }
-        
-        String chanName = chan.getName();
-        String acct = IRCUtils.getAccount(user);
-        
-        registrar.put(chanName, register(registrar.get(chanName), acct, level));
     }
 
     private Map<String, PermLevel> register(Map<String, PermLevel> curChanMap, String acct, PermLevel level)
     {
         if (curChanMap == null)
             curChanMap = new HashMap<String, PermLevel>();
-        
+
         curChanMap.put(acct, level);
         return curChanMap;
     }
-    
+
     public PermLevel getPermLevelForUser(Channel chan, User user)
     {
         String acct = IRCUtils.getAccount(user);
-        
+
         if (controllers.contains(acct))
             return PermLevel.CONTROLLER;
-        
+
         if (!registrar.containsKey(chan.getName()))
             return PermLevel.DEFAULT;
-        
+
         PermLevel perm = registrar.get(chan.getName()).get(acct);
         return perm == null ? PermLevel.DEFAULT : perm;
     }
-    
+
     @Subscribe
     public void onDisconnect(DisconnectEvent<PircBotX> event)
     {
