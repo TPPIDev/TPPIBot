a41dd82a00302c1e43883e0cfd34334a4cf8d685
tterrag1098 <tterrag1098@gmail.com>
2014-09-15 13:11:16 -0400

    Fix drama command

--- src/main/java/tterrag/tppibot/commands/Drama.java
+++ src/main/java/tterrag/tppibot/commands/Drama.java
@@ -27,20 +27,17 @@ public class Drama extends Command
     {
         try
         {
-            URI uri = new URI("http://asie.pl/drama.php");
+            URI uri = args.length >= 1 && args[0].equals("2") ? new URI("http://asie.pl/drama.php?2&plain") : new URI("http://asie.pl/drama.php?plain");
             HttpURLConnection connection = (HttpURLConnection) uri.toURL().openConnection();
 
             BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-            String s = "";
-            String drama = null;
-            while ((s = in.readLine()) != null)
+            String drama = in.readLine();
+            int end = drama.indexOf("<");
+            if (end > 0)
             {
-                if (s.startsWith("<h1>"))
-                {
-                    drama = s.substring(4, s.length() - 5);
-                    break;
-                }
+                drama = drama.substring(0, drama.indexOf("<"));
             }
+
             if (channel != null)
             {
                 // ping avoiding
@@ -57,7 +54,7 @@ public class Drama extends Command
                 }
                 drama = StringUtils.join(words, " ");
             }
-            
+
             MessageSender.instance.enqueue(bot, channel == null ? user.getNick() : channel.getName(), drama);
         }
         catch (Exception e)
