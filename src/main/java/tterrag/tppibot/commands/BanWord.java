5351c11d08f173a4a97667418d106942a79a2d64
TehNut <nick_ignoffo@yahoo.com>
2015-06-26 20:04:48 -0700

    Trusted command to print banned word list to a notice

--- src/main/java/tterrag/tppibot/commands/BanWord.java
+++ src/main/java/tterrag/tppibot/commands/BanWord.java
@@ -7,36 +7,64 @@ import org.pircbotx.PircBotX;
 import org.pircbotx.User;
 
 import tterrag.tppibot.Main;
+import tterrag.tppibot.registry.PermRegistry;
+import tterrag.tppibot.runnables.MessageSender;
+import tterrag.tppibot.util.IRCUtils;
 
 public class BanWord extends Command
 {
     public BanWord()
     {
-        super("banned", PermLevel.CONTROLLER);
+        super("banned", PermLevel.TRUSTED);
     }
     
     @Override
     public void onCommand(PircBotX bot, User user, Channel channel, List<String> lines, String... args)
     {
+        PermLevel level = PermRegistry.instance().getPermLevelForUser(channel, user);
+
         if (args.length < 2)
         {
-            lines.add("This command requires 2 args, [action] and [word]");
-            return;
+            if (args.length > 0)
+            {
+                if (!args[0].equals("list"))
+                {
+                    lines.add("This command requires 2 args, [action] and [word]");
+                    return;
+                }
+                else
+                {
+                    String list = "";
+
+                    for (String word : Main.bannedWords.getBannedWords())
+                        list += list.length() == 0 ? word : ", " + word;
+
+                    MessageSender.instance.enqueueNotice(bot, user.getNick(), list);
+                    return;
+                }
+            }
+            else
+            {
+                lines.add("This command requires 2 args, [action] and [word]");
+                return;
+            }
         }
-        
-        if ("add".equals(args[0]))
+
+        if (IRCUtils.isPermLevelAboveOrEqualTo(level, PermLevel.CONTROLLER))
         {
-            Main.bannedWords.addWord(args[1]);
-            lines.add("Successfully banned word.");
-        }
-        else if ("remove".equals(args[0]))
-        {
-            Main.bannedWords.removeWord(args[1]);
-            lines.add("Successfully removed word.");
+            if ("add".equals(args[0])) {
+                Main.bannedWords.addWord(args[1]);
+                lines.add("Successfully banned word.");
+            } else if ("remove".equals(args[0])) {
+                Main.bannedWords.removeWord(args[1]);
+                lines.add("Successfully removed word.");
+            } else {
+                lines.add(args[0] + " is not a valid action.");
+            }
         }
         else
         {
-            lines.add(args[0] + " is not a valid action.");
+            MessageSender.instance.enqueueNotice(bot, user.getNick(), "You are not a " + PermLevel.CONTROLLER.toString());
         }
     }
 }
