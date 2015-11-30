72e75962726cbf5a4a761eb6776fcfc7b93995f0
Nick <speedynutty68@gmail.com>
2015-11-30 02:28:59 -0800

    Configurable entries for Rekt command

--- src/main/java/tterrag/tppibot/commands/Rekt.java
+++ src/main/java/tterrag/tppibot/commands/Rekt.java
@@ -1,16 +1,22 @@
 package tterrag.tppibot.commands;
 
+import com.google.common.base.Strings;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 import org.pircbotx.Channel;
 import org.pircbotx.PircBotX;
 import org.pircbotx.User;
+import org.pircbotx.hooks.events.DisconnectEvent;
+import tterrag.tppibot.annotations.Subscribe;
+import tterrag.tppibot.config.Config;
 
-import java.util.List;
-import java.util.Random;
+import java.util.*;
 
 public class Rekt extends Command {
 
-    String[] rektList = {
-            "\u2610 Not REKT" ,
+    private final String PREFIX = "\u2610 Not REKT";
+
+    private final String[] REKT_DEFAULT = {
             "\u2611 REKT",
             "\u2611 REKTangle",
             "\u2611 SHREKT",
@@ -29,19 +35,76 @@ public class Rekt extends Command {
             "\u2611 Harry Potter: The Half-Rekt Prince"
     };
 
+    private ArrayList<String> rektEntries = new ArrayList<>();
+
+    private Config rektConfig;
+
     public Rekt() {
         super("rekt");
+
+        rektConfig = new Config("rektEntries.json");
+
+        if (Strings.isNullOrEmpty(rektConfig.getText())) {
+            rektEntries.addAll(Arrays.asList(REKT_DEFAULT));
+            rektConfig.writeJsonToFile(rektEntries);
+        } else {
+            rektEntries = new Gson().fromJson(rektConfig.getText(), new TypeToken<ArrayList<String>>() {}.getType());
+        }
     }
 
     @Override
     public void onCommand(PircBotX bot, User user, Channel channel, List<String> lines, String... args) {
         Random random = new Random();
 
-        lines.add(rektList[random.nextInt(rektList.length)]);
+        if (args.length == 0) {
+            lines.add(String.format("%s %s", PREFIX, rektEntries.get(random.nextInt(rektEntries.size()))));
+        } else if(args.length == 1 && args[0].equals("list")) {
+            String ret = "";
+            for (String entry : rektEntries)
+                ret += (ret.length() > 0 ? ", " : "") + entry;
+
+            lines.add(ret);
+        } else if (args.length >= 2) {
+            String rektString = "";
+
+            for (int i = 1; i < args.length; i++)
+                rektString += (rektString.length() > 0 ? " " : "") + args[i];
+
+            switch (args[0]) {
+                case "add": {
+                    if (!rektEntries.contains(rektString)) {
+                        rektEntries.add(rektString);
+                        lines.add(String.format("Successfully added entry \"%s\"", rektString));
+                    }else {
+                        lines.add(String.format("Could not add entry \"%s\"", rektString));
+                    }
+                    break;
+                }
+                case "del": {
+                    if (rektEntries.contains(rektString)) {
+                        rektEntries.remove(rektString);
+                        lines.add(String.format("Successfully removed entry \"%s\"", rektString));
+                    } else {
+                        lines.add(String.format("Could not remove entry \"%s\"", rektString));
+                    }
+                    break;
+                }
+            }
+        }
     }
 
     @Override
     public String getDesc() {
         return "Returns the state in which the user is.";
+    }
+
+    @Override
+    public boolean shouldReceiveEvents() {
+        return true;
+    }
+
+    @Subscribe
+    public void onDisconnect(DisconnectEvent<PircBotX> event) {
+        rektConfig.writeJsonToFile(rektEntries);
     }
 }
